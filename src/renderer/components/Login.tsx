b20aa61a72119e09f61277fd2090a13a4a1521d2
Zayne Komichi <zaynekomichi@hotmail.com>
2021-12-04 17:20:09 +0200

    Login design done

--- src/renderer/components/Login.tsx
+++ src/renderer/components/Login.tsx
@@ -1,8 +1,20 @@
+import {useHistory} from 'react-router-dom';
 import './Login.css';
+import Logo from '../../../assets/doctor.png';
+import back from '../../../assets/DLogin.jpg';
 const Login = () => {
+  const Verify =()=>{
+    let history = useHistory();
+    history.push("/Main");
+  }
   return(
+    <div className="LoginContainer">
+    <div className="Login">
+      <img src={back} className="bg"/>
+    </div>
     <div className="LoginMain">
       <div className="Header">
+      <img src={Logo} alt="" className="Logo"/>
         <h1>AURORA</h1>
       </div>
       <div className="LoginForm">
@@ -11,9 +23,12 @@ const Login = () => {
         <span>Password</span>
         <input type="Password"/>
       </div>
-      <div>
-        <button className="">Login</button>
+      <div className="Button">
+       <button className="ButtonLogin" onClick={()=>{
+        Verify();
+       }}><a href="/Main">Login</a></button>
       </div>
+    </div>
     </div>
   );
 };
---
+++ src/renderer/components/Main.tsx
@@ -0,0 +1,7 @@
+import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
+
+export default function Main() {
+  return (
+   <div>AURORA</div>
+  );
+}
